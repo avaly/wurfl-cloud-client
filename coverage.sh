9711388461a8227edeb87d343d5585323b27bf93
Valentin Agachi <github-com@agachi.name>
2014-07-10 19:06:09 +0200

    Fix coverage script for Travis

--- coverage.sh
+++ coverage.sh
@@ -1,11 +1,16 @@
-#/bin/bash
+#!/bin/bash
 
 istanbul instrument --output lib-cov lib
 
 COVERAGE=1 ISTANBUL_REPORTERS="text-summary,lcovonly" mocha --reporter mocha-istanbul
 
-cat lcov.info | node_modules/coveralls/bin/coveralls.js
-genhtml -s -o lcov-report lcov.info
+if [[ "$TRAVIS" == "true" ]]; then
+	cat lcov.info | node_modules/coveralls/bin/coveralls.js
+fi
+
+if [[ "$TRAVIS" != "true" ]]; then
+	genhtml -s -o lcov-report lcov.info
+fi
 
 rm lcov.info
 rm -rf lib-cov
